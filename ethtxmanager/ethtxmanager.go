1f0d894da4bcdb1bc44fd4126a573b4e91eb401c
marius <wangone2008@gmail.com>
2023-05-29 18:51:35 +0800

    fix

--- ethtxmanager/ethtxmanager.go
+++ ethtxmanager/ethtxmanager.go
@@ -76,7 +76,11 @@ func (c *Client) Add(ctx context.Context, owner, id string, from common.Address,
 	// get gas
 	gas, err := c.etherman.EstimateGas(ctx, from, to, value, data)
 	if err != nil {
-		err := fmt.Errorf("failed to estimate gas: %w, data: %v", err, common.Bytes2Hex(data))
+		latestBlockNumber, err := c.etherman.GetLatestBlockNumber(ctx)
+		if err != nil {
+			return fmt.Errorf("failed to estimate gas. err: %v", err)
+		}
+		err = fmt.Errorf("failed to estimate gas: %v, data: %v, latestBlockNumber = %d", err, common.Bytes2Hex(data), latestBlockNumber)
 		log.Error(err.Error())
 		if c.cfg.ForcedGas > 0 {
 			gas = c.cfg.ForcedGas
